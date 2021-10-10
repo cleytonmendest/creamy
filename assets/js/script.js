38a25ccc2d7ca475fae5be36876dbd1e20c28932
Cleyton Mendes <cleyton.mendest@gmail.com>
2021-10-10 14:39:58 -0300

    inclusao area carrinho

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,19 @@
 const search = document.querySelector('.search');
 const loupe = document.querySelector('.loupe');
+const menu = document.querySelector('#menu');
+const sticky = menu.offsetTop;
+const cart = document.querySelector('.cart');
 
-//TOGGLE INPUT BUSCAR
+// sticky menu
+window.onscroll = () =>{
+    if(window.pageYOffset >= sticky){
+        menu.classList.add("sticky");
+    }else{
+        menu.classList.remove("sticky");
+    }
+}
+
+//TOGGLE INPUT BUSCAR - CARRINHO
 document.addEventListener('click', (e) => {
     const el = e.target;
     if(el.classList.contains("loupe")){
@@ -11,16 +23,11 @@ document.addEventListener('click', (e) => {
             search.classList.add("active");
         }
     }
-})
-
-// sticky menu
-const menu = document.querySelector('#menu');
-const sticky = menu.offsetTop;
-
-window.onscroll = () =>{
-    if(window.pageYOffset >= sticky){
-        menu.classList.add("sticky");
-    }else{
-        menu.classList.remove("sticky");
+    if(el.classList.contains("cart-img") || el.classList.contains("cart") || el.classList.contains("close")){
+        if(cart.classList.contains("active")){
+            cart.classList.remove("active");
+        }else{
+            cart.classList.add("active");
+        }
     }
-}
+})
